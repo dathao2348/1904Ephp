0aa825f5b0ec01700ba1426093b5ceb18208b607
admin <tranthidathao234@gmail.com>
2019-07-27 20:35:32 +0700

    234

---
+++ includevarequire/001/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$cv = tinhchuvihinhtron(3);
+
+/*
+ * Fatal error: Uncaught Error: Call to undefined function tinhchuvihinhtron() in C:\xampp\htdocs\1904Ephp\includevarequire\001\index.php:3 Stack trace: #0 {main} thrown in C:\xampp\htdocs\1904Ephp\includevarequire\001\index.php on line 3
+ * */
+echo "Chu vi : " , $cv;
---
+++ includevarequire/004/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * câu lệnh include " đường dẫn file";
+ * thì có thể sử dụng những hàm hoặc class hoặc biến hoặc hằng số của file các bạn vừa nạp
+ */
+require "functions.php";
+$cv= tinhchuvihinhtron(3);
+
+echo "Chu vi : ", $cv;
