d796bd4be47a85bf8f75ff185e2384ba7a061eec
admin <tranthidathao234@gmail.com>
2019-08-05 20:24:23 +0700

    234

--- M9.APPCRUD/config.php
+++ M9.APPCRUD/config.php
@@ -1,7 +1,15 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: admin
- * Date: 8/5/2019
- * Time: 07:51 PM
+ * Khai báo 4 hằng số kết nối CSDL
  */
+define("DB_SERVER","localhost");
+define("DB_USER","root");
+define("DB_PASSWORD","");
+define("DB_NAME","appcrud1");
+/**
+ * Kết nối CSDL
+ */
+$connection=mysqli_connect(DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME);
+if($connection->connect_error == true){
+    die("KHhoongtheer kết nối đến CSDL");
+}
